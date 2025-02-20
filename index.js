0796fb11d87840a994967f6d97c36acb2cda7553
MH Nayem <mhnayem79@gmail.com>
2025-02-20 19:24:14 +0600

    all cars load from database

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors());
 app.use(express.json());
@@ -26,6 +26,30 @@ async function run() {
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+        // cars related apis
+        const carsCollection = client.db('cars-collection').collection('cars')
+        // recent cars added shows on homepage
+        app.get('/cars', async (req, res) => {
+            const cursor = carsCollection.find().limit(6);
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+        // available all cars shows on available car
+        app.get('/allCars', async (req, res) => {
+            const cursor = carsCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        // see details by id and get a single document
+        app.get('/allCars/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await carsCollection.findOne(query);
+            res.send(result);
+        })
+
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
